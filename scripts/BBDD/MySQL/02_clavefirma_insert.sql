97d491db00d44a8e76c5a2c5291d1516f91707d4
Adolfo Navarro <adolfo.navarro@ricoh.es>
2017-12-26 11:16:55 +0100

    se modifica nombre

--- scripts/BBDD/MySQL/02_clavefirma_insert.sql
+++ scripts/BBDD/MySQL/02_clavefirma_insert.sql
@@ -4,5 +4,5 @@
 
 insert into tb_usuarios (nombre_usuario,clave,nombre,apellidos) 
 
-values('admin_pass','D/4avRoIIVNTwjPW4AlhPpXuxCU4Mqdhryj/N6xaFQw=','dafaul name','default surnames');
+values('admin_pass','D/4avRoIIVNTwjPW4AlhPpXuxCU4Mqdhryj/N6xaFQw=','default name','default surnames');
 --COMMIT
