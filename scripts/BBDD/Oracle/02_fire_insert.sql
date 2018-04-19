6d9548fe7158d7b6ec83bac57f013581fa441ed1
Carlos <carlos.gamuci@gmail.com>
2018-04-19 15:32:32 +0200

    Correcciones BD
    - Se corrigen los scripts de base de datos incluyendo ';' donde era necesario y un commit al final de los scripts que modificaban el contenido de las tablas.

--- scripts/BBDD/Oracle/02_fire_insert.sql
+++ scripts/BBDD/Oracle/02_fire_insert.sql
@@ -6,4 +6,4 @@ INSERT INTO TB_USUARIOS (nombre_usuario,clave,nombre,apellidos,usu_defecto)
 
 VALUES('admin','D/4avRoIIVNTwjPW4AlhPpXuxCU4Mqdhryj/N6xaFQw=','default name','default surnames',1);
 
---COMMIT
+COMMIT;
