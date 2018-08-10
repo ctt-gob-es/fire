8ba94e091bd1ac4fae9b811adc1f74ae0367e3ca
Carlos <carlos.gamuci@gmail.com>
2018-08-10 13:10:51 +0200

    Log al fallar la carga del fichero
    - Se imprime la traza de error completa al fallar la carga del fichero de configuración del componente central.

--- fire-test-jsp/src/main/java/es/gob/fire/test/webapp/ConfigManager.java
+++ fire-test-jsp/src/main/java/es/gob/fire/test/webapp/ConfigManager.java
@@ -11,6 +11,7 @@ package es.gob.fire.test.webapp;
 
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import es.gob.fire.client.ClientConfigFilesNotFoundException;
@@ -66,7 +67,7 @@ public class ConfigManager {
 			this.config.load(is);
 		}
 		catch (final Exception e) {
-			LOGGER.warning("No se ha podido cargar la configuracion de la aplicacion: " + e); //$NON-NLS-1$
+			LOGGER.log(Level.WARNING, "No se ha podido cargar la configuracion de la aplicacion: " + e, e); //$NON-NLS-1$
 		}
 		try {
 			is.close();
