f83500e205d072d350c5533a91aa915e0c93b360
Carlos Gamuci <carlos.gamuci@gmail.com>
2019-11-28 10:20:16 +0100

    Compartición de sesiones en BD
    - Se crea un script independiente para crear las tablas necesarias para permitir que se compartan a través de base de datos las sesiones de diversos nodos con el componente central de FIRe.

---
+++ scripts/BBDD/MySQL/Opcional_sesiones_en_bd_ddl.sql
@@ -0,0 +1,22 @@
+-- ********************************************************
+-- *** Tablas para el guardado de sesiones y documentos ***
+-- *** en base de datos. Solo necesarias si se activa   ***
+-- *** esta opcion en el fichero de configuracion del   ***
+-- *** componente central.                              ***
+-- ********************************************************
+
+-- Tabla para el guardado temporal de sesiones en BD
+CREATE TABLE `tb_sesiones` (
+  `id` varchar(64) NOT NULL,
+  `f_modificacion` bigint(10) NOT NULL,
+  `sesion` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Tabla para el guardado temporal de documentos en BD
+CREATE TABLE `tb_documentos` (
+  `id` varchar(100) NOT NULL,
+  `f_modificacion` bigint(10) NOT NULL,
+  `datos` longblob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
