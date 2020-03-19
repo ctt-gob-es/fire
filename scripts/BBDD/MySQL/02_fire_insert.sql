1fbfc8fe6f901d5c7ce70ffdc72261b6dab59a3d
Carlos Gamuci <carlos.gamuci@gmail.com>
2020-03-19 19:39:20 +0100

    Scripts MySQL
    - Se corrigen y actualizan los scripts para la creación de la BD en MySQL y el de migración desde una versión anterior de FIRe.

--- scripts/BBDD/MySQL/02_fire_insert.sql
+++ scripts/BBDD/MySQL/02_fire_insert.sql
@@ -4,18 +4,15 @@
 
 -- INSERTAR DATOS--------------
 
+-- ROLES --------
+INSERT INTO `tb_roles` (`id`,`nombre_rol`,`permisos`) 
+VALUES (1,'admin','1,2'),
+	   (2,'responsible','2'),
+	   (3,'contact', NULL);
+
 -- USUARIO POR DEFECTO --------
-insert into tb_usuarios (nombre_usuario,clave,nombre,apellidos,usu_defecto) 
-values('admin','D/4avRoIIVNTwjPW4AlhPpXuxCU4Mqdhryj/N6xaFQw=','default name','default surnames',1);
+INSERT INTO tb_usuarios (nombre_usuario,clave,nombre,apellidos,usu_defecto,fk_rol) 
+VALUES('admin','D/4avRoIIVNTwjPW4AlhPpXuxCU4Mqdhryj/N6xaFQw=','default name','default surnames',1,1);
 
--- ROLES INICIALES --------
-insert into tb_roles (nombre_rol,permisos) 
-values('admin','1,2');
-
-insert into tb_roles (nombre_rol,permisos) 
-values('responsible','1');
-
-insert into tb_roles (nombre_rol) 
-values('contact');
 
 COMMIT;
--- scripts/BBDD/MySQL/Migracion_fire_2_2_-_2_4.sql
+++ scripts/BBDD/MySQL/Migracion_fire_2_2_-_2_4.sql
@@ -1,9 +1,8 @@
--- Script de migracion desde FIRe 2.2 o 2.3 a 2.4
+-- Script de migracion desde FIRe 2.2/2.3 a 2.4
 
--- 1 Crear la tablas nuevas 
 
 -- Tabla para el guardado de las referencias a los servidores de log
-
+SET character_set_client = utf8mb4 ;
 CREATE TABLE `tb_servidores_log` (
   `id_servidor` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
@@ -15,8 +14,9 @@ CREATE TABLE `tb_servidores_log` (
   UNIQUE KEY `url_servicio_log_UNIQUE` (`url_servicio_log`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- Tabla para el guardado de las estadisticas de las firmas
+-- Tabla de las estadisticas de las firmas
 
+SET character_set_client = utf8mb4 ;
 CREATE TABLE `tb_firmas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fecha` datetime DEFAULT NULL COMMENT 'Fecha de la operacion',
@@ -31,8 +31,9 @@ CREATE TABLE `tb_firmas` (
   PRIMARY KEY (`id`,`navegador`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- Tabla para el guardado de las estadisticas de las transacciones
+-- Tabla de estadisticas de las transacciones
 
+SET character_set_client = utf8mb4 ;
 CREATE TABLE `tb_transacciones` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fecha` datetime DEFAULT NULL COMMENT 'Fecha de la operacion',
@@ -46,8 +47,10 @@ CREATE TABLE `tb_transacciones` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- Tabla para el guardado de los roles de los usuarios
 
+-- Tabla de roles
+
+SET character_set_client = utf8mb4 ;
 CREATE TABLE `tb_roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_rol` varchar(45) NOT NULL,
@@ -57,118 +60,75 @@ CREATE TABLE `tb_roles` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
---creamos la tabla sin asociar nada y luego la modificamos
+--   Insertamos los permisos de los roles
+INSERT INTO `tb_roles` (`id`,`nombre_rol`,`permisos`) 
+VALUES (1,'admin','1,2'),
+	   (2,'responsible','2'),
+	   (3,'contact', NULL);
+
+
+
+-- TABLA DE USUARIOS
+
+-- Agregamos los campos necesarios
+ALTER TABLE `tb_usuarios`
+ADD `fk_rol` int(11) NOT NULL DEFAULT 1 AFTER `telf_contacto`,
+ADD `codigo_renovacion` VARCHAR(90) UNIQUE DEFAULT NULL,
+ADD `fec_renovacion` datetime DEFAULT NULL,
+ADD `rest_clave` tinyint(4) DEFAULT 0;
+ 
+-- Eliminamos los campos sobrantes
+ALTER TABLE `tb_usuarios`
+DROP rol;
+
+-- Modificamos la columna de clave para que pueda ser nula (los usuarios que no tengan permisos de acceso)
+ALTER TABLE `tb_usuarios`
+MODIFY `clave` VARCHAR (45) NULL;
+
+-- Creamos la clave foranea con la tabla de roles
+ALTER TABLE `tb_usuarios` 
+ADD CONSTRAINT `fk_rol`
+FOREIGN KEY (`fk_rol`)
+  REFERENCES `tb_roles` (`id`)
+ON DELETE RESTRICT
+ ON UPDATE CASCADE;
+
+-- Insertamos como usuarios a los responsables declarados en las aplicaciones
+INSERT INTO `tb_usuarios` (`nombre_usuario`, `nombre`, `apellidos`, `correo_elec`, `telf_contacto`, `fec_alta`, `fk_rol`)
+SELECT SUBSTRING(`id`, 1, 30), `responsable`, `responsable`, `resp_correo`, `resp_telefono`, `fecha_alta`, 2
+FROM `tb_aplicaciones`;
+
+
+-- Tabla de relacion de responsables y aplicaciones
+
+SET character_set_client = utf8mb4 ;
 CREATE TABLE `tb_responsable_de_aplicaciones` (
   `id_responsables` int(11) NOT NULL ,
   `id_aplicaciones` varchar(48) NOT NULL
   
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+ALTER TABLE tb_responsable_de_aplicaciones ADD PRIMARY KEY ( id_responsables , id_aplicaciones );
 
-ALTER TABLE tb_responsable_de_aplicaciones ADD PRIMARY KEY ( id_responsables , id_aplicaciones ) 
-
-
-
---   Insertamos los permisos del usuario al inicializar la aplicacion
-insert into `tb_roles` (`nombre_rol`,`permisos`) 
-values('admin','1,2');
-
-insert into `tb_roles` (`nombre_rol`,`permisos`) 
-values('responsible','1');
-
-insert into `tb_roles` (`nombre_rol`) 
-values('contact');
+-- Agregamos la relacion entre las aplicaciones y los usuarios responsables de ellas
+INSERT INTO `tb_responsable_de_aplicaciones` (`id_responsables`, `id_aplicaciones`)
+SELECT `id_usuario`, `nombre_usuario`
+FROM `tb_usuarios`
+WHERE `fk_rol` = 2;
 
 
 
--- Agregamos campos a una tabla
--- Aplicaciones 
-
-ALTER TABLE `tb_aplicaciones` ADD `fk_responsable` int(11) NOT NULL;
- 
-ALTER TABLE `tb_aplicaciones` ADD `habilitado` tinyint(4) DEFAULT 0;
- 
-ALTER TABLE `tb_aplicaciones` 
-ADD INDEX `fk_responsable_idx` (`nombre` ASC);
-
- ALTER TABLE `tb_aplicaciones` 
-ADD INDEX `fk_responsable_idx1` (`fk_responsable` ASC);
-
-
--- Usuarios
-
-ALTER TABLE `tb_usuarios` ADD `codigo_renovacion` VARCHAR(90) DEFAULT NULL
-AFTER `usu_defecto`;
- 
-ALTER TABLE `tb_usuarios` ADD `fec_renovacion` datetime DEFAULT NULL
- AFTER `codigo_renovacion`;
- 
-ALTER TABLE `tb_usuarios` ADD `rest_clave` tinyint(4) DEFAULT 0
- AFTER `fec_renovacion`;
- 
+-- TABLA APLICACIONES
   
- ALTER TABLE `tb_usuarios`
-ADD UNIQUE (`codigo_renovacion`);
+-- Eliminamos los campos del responsable
+ALTER TABLE `tb_aplicaciones`
+DROP responsable,
+DROP resp_correo,
+DROP resp_telefono;
 
--- Agregar campo "fk_rol"
-
-ALTER TABLE `tb_usuarios` ADD `fk_rol` int(11) NOT NULL
- AFTER `usu_defecto`;
- 
- 
--- Asignar rol administrador a todos los usuarios en el campo "fk_rol"
-
-
-UPDATE `tb_usuarios`SET `fk_rol`=1;
-
-
--- Eliminar campo "rol"
-
-ALTER TABLE `tb_usuarios` DROP COLUMN rol;
-
--- Modificamos la columna para que sea null
-
-ALTER TABLE `tb_usuarios` MODIFY `clave` VARCHAR (45) NULL;
-
-
--- Insertamos campos a una tabla
-INSERT INTO `tb_usuarios` (`nombre_usuario`, `nombre`, `apellidos`, `correo_elec`, `telf_contacto`, `fk_rol`)
-SELECT `id`, `responsable`, `responsable`, `resp_correo`, `resp_telefono`, 3
-FROM `tb_aplicaciones`;
-
--- Actualizamos los campos de responsable 
-
-UPDATE `tb_aplicaciones`SET `fk_responsable`=1;
-
--- Creamos las foreign key
- 
- ALTER TABLE `tb_aplicaciones`
- ADD CONSTRAINT `fk_responsable`
- FOREIGN KEY (`fk_responsable`)
-  REFERENCES `tb_usuarios` (`id_usuario`)
-  ON DELETE RESTRICT 
-  ON UPDATE CASCADE;
-  
-  
-  
-   ALTER TABLE `tb_usuarios` 
-ADD CONSTRAINT `fk_rol`
-FOREIGN KEY (`fk_rol`)
-  REFERENCES `tb_roles` (`id`)
-ON DELETE RESTRICT
- ON UPDATE CASCADE;
- 
- -- Creamos el idx del rol
- 
- ALTER TABLE `tb_usuarios` 
-ADD INDEX `fk_rol_idx` (`fk_rol` ASC);
-
-
-  
--- Elimina campos de una tabla
-
-ALTER TABLE `tb_aplicaciones` DROP COLUMN responsable, DROP COLUMN resp_correo, DROP COLUMN resp_telefono;
-
+-- Agregamos el campo de 'habilitado' dejando las aplicaciones habilitadas por defecto
+ALTER TABLE `tb_aplicaciones`
+ADD `habilitado` tinyint(4) DEFAULT 1;
 
 
 
