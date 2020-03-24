1d04f2b1acf6b5e96a58d8d2e86e955dc8e2f430
Carlos Gamuci <carlos.gamuci@gmail.com>
2020-03-24 12:40:01 +0100

    Corrección de compilación
    - Se sube un fichero faltante para permitir la compilación.

---
+++ fire-upgrade/src/main/java/es/gob/fire/upgrade/UpgradeParams.java
@@ -0,0 +1,9 @@
+package es.gob.fire.upgrade;
+
+public class UpgradeParams {
+
+	/**
+	 * Indica que se permite
+	 */
+	public static final String ALLOW_PARTIAL_UPGRADE = "allowPartialUpgrade"; //$NON-NLS-1$
+}
