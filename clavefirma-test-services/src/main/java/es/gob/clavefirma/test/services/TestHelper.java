0faf31377399f9296fbd7d239b5b96d72c493933
Carlos Gamuci <carlos.gamuci@gmail.com>
2021-12-22 09:13:27 +0100

    Configuración con variables de entorno
    - Se permite el uso de variables de entorno para establecer los valores del fichero de configuración del simulador de pruebas de Cl@ve Firma.

--- clavefirma-test-services/src/main/java/es/gob/clavefirma/test/services/TestHelper.java
+++ clavefirma-test-services/src/main/java/es/gob/clavefirma/test/services/TestHelper.java
@@ -36,9 +36,11 @@ class TestHelper {
 
 	private static final String SYS_PROP_PREFIX = "${"; //$NON-NLS-1$
 
-	private static final String SYS_PROP_SUFIX = "}"; //$NON-NLS-1$
+	private static final String ENV_PROP_PREFIX = "%{"; //$NON-NLS-1$
 
-	private static final String DEFAULT_ENCODING = "utf-8"; //$NON-NLS-1$
+	private static final String PROP_SUFIX = "}"; //$NON-NLS-1$
+
+		private static final String DEFAULT_ENCODING = "utf-8"; //$NON-NLS-1$
 
 	/** Nombre del fichero de configuraci&oacute;n. */
 	private static final String CONFIG_FILE = "test-backend.properties" ; //$NON-NLS-1$
@@ -191,7 +193,7 @@ class TestHelper {
 		// Expandimos las propiedades configuradas con los parametros proporcionados
 		// en el arranque del servidor
 		for (final String key : config.keySet().toArray(new String[0])) {
-			config.setProperty(key, mapSystemProperties(config.getProperty(key)));
+			config.setProperty(key, mapProperties(config.getProperty(key)));
 		}
 
 		return config;
@@ -399,13 +401,12 @@ class TestHelper {
 
 	/**
 	 * Mapea las propiedades del sistema que haya en el texto que se referencien de
-	 * la forma: ${propiedad}
-	 * @param text Texto en el que se pueden encontrar las referencias a las propiedades
-	 * del sistema.
-	 * @return Cadena con las part&iacute;culas traducidas a los valores indicados como propiedades
-	 * del sistema. Si no se encuentra la propiedad definida, no se modificar&aacute;.
+	 * la forma ${propiedad} y las variables de entorno referenciadas como %{propiedad}.
+	 * @param text Texto en el que se pueden encontrar las referencias a las propiedades.
+	 * @return Text de entrada con las propiedades sustituidas por sus valores correspondientes.
+	 * Si no se encuentra la propiedad definida, no se modificar&aacute;.
 	 */
-	private static String mapSystemProperties(final String text) {
+	private static String mapProperties(final String text) {
 
 		if (text == null) {
 			return null;
@@ -414,16 +415,33 @@ class TestHelper {
 		int pos = -1;
 		int pos2 = 0;
 		String mappedText = text;
+
+		// Mapeamos las variables de sistema
 		while ((pos = mappedText.indexOf(SYS_PROP_PREFIX, pos + 1)) > -1 && pos2 > -1) {
-			pos2 = mappedText.indexOf(SYS_PROP_SUFIX, pos + SYS_PROP_PREFIX.length());
+			pos2 = mappedText.indexOf(PROP_SUFIX, pos + SYS_PROP_PREFIX.length());
 			if (pos2 > pos) {
 				final String prop = mappedText.substring(pos + SYS_PROP_PREFIX.length(), pos2);
 				final String value = System.getProperty(prop, null);
 				if (value != null) {
-					mappedText = mappedText.replace(SYS_PROP_PREFIX + prop + SYS_PROP_SUFIX, value);
+					mappedText = mappedText.replace(SYS_PROP_PREFIX + prop + PROP_SUFIX, value);
 				}
 			}
 		}
+
+		// Mapeamos las variables de entorno
+		pos = -1;
+		pos2 = 0;
+		while ((pos = mappedText.indexOf(ENV_PROP_PREFIX, pos + 1)) > -1 && pos2 > -1) {
+			pos2 = mappedText.indexOf(PROP_SUFIX, pos + ENV_PROP_PREFIX.length());
+			if (pos2 > pos) {
+				final String prop = mappedText.substring(pos + ENV_PROP_PREFIX.length(), pos2);
+				final String value = System.getenv(prop);
+				if (value != null) {
+					mappedText = mappedText.replace(ENV_PROP_PREFIX + prop + PROP_SUFIX, value);
+				}
+			}
+		}
+
 		return mappedText;
 	}
 
