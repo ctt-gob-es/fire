c7f2f6470f9bacc1d6478175d9883d59d29699b6
JairoFJ <jairo.figueroa@ricoh.es>
2025-01-24 09:56:52 +0100

    BB26983-676: implementación de login con pasarela clave, campo dni y modal con información acerca del último login

--- fire-admin-web/src/main/java/es/gob/fire/web/clave/sp/utils/SPConfig.java
+++ fire-admin-web/src/main/java/es/gob/fire/web/clave/sp/utils/SPConfig.java
@@ -1,20 +1,21 @@
 package es.gob.fire.web.clave.sp.utils;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.Properties;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import es.gob.fire.commons.utils.UtilsServer;
 /**
  * Utility class for Service Provider configurations.
  */
 public class SPConfig {
     
-    /**
+	/**
      * Constructor for SPUtil. Private to prevent instantiation.
      */
     SPConfig() {}
@@ -24,24 +25,21 @@ public class SPConfig {
      */
     private static final Logger LOG = LoggerFactory.getLogger(SPConfig.class);
     
-    private static final String CLAVE_DIRECTORY = "clave";
-    
     /**
-     * Retrieves the configuration file path from the resources directory.
+     * Retrieves the configuration file path.
      *
      * @return The configuration file path as a String.
-     * @throws IllegalStateException If the resource directory is not found.
      */
     public static String getConfigFilePath() {
-        URL resource = SPConfig.class.getClassLoader().getResource(CLAVE_DIRECTORY);
-        if (resource == null) {
-            throw new IllegalStateException("Resource directory 'clave' not found in the classpath.");
-        }
-        return resource.getPath();
+        // Commented out code that retrieves the path from environment or system properties.
+        /*String envLocation = System.getenv().get(Constants.SP_CONFIG_REPOSITORY);
+        String configLocation = System.getProperty(Constants.SP_CONFIG_REPOSITORY, envLocation);
+        return configLocation;*/
+    	return UtilsServer.createAbsolutePath(UtilsServer.getServerConfigDir(), UtilsServer.CLAVE_DIRECTORY) + File.separator;
     }
     
     /**
-     * Loads configurations from a specified file in the resources/clave directory.
+     * Loads configurations from a specified file.
      *
      * @param fileName The name of the configuration file.
      * @return A Properties object containing the configurations.
@@ -49,18 +47,19 @@ public class SPConfig {
      */
     private static Properties loadConfigs(String fileName) throws IOException {
         Properties properties = new Properties();
+        FileReader fileReader = null;
         
-        // Usa el ClassLoader para cargar el archivo del classpath
-        try (InputStream inputStream = SPConfig.class.getClassLoader().getResourceAsStream(CLAVE_DIRECTORY + File.separator + fileName)) {
-            if (inputStream == null) {
-                throw new IOException("Configuration file not found: clave/" + fileName);
-            }
-            properties.load(inputStream);
+        try {
+            File f = new File(SPConfig.getConfigFilePath() + fileName);
+            fileReader = new FileReader(f);
+            properties.load(fileReader);
+        } finally {
+            IOUtils.closeQuietly(fileReader);
         }
         
         return properties;
     }
-
+    
     /**
      * Loads Service Provider configurations from the default properties file.
      *
@@ -71,7 +70,7 @@ public class SPConfig {
         try {
             result = SPConfig.loadConfigs(Constants.CLAVE_CONFIG_PROPERTIES);
         } catch (IOException e) {
-            LOG.error("Failed to load Service Provider configurations from properties file.", e);
+            LOG.error(e.getMessage(), e);
         }
         return result;
     }
