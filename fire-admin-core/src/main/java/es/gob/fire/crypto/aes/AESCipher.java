13d2cbd4b8b7dbe480ed2ceb232d0e211857a8d2
Carlos Gamuci <carlos.gamuci@gmail.com>
2025-08-14 11:30:48 +0200

    Corrección acceso almacenes
    - Se resuelve el problema en la decodificación de la contraseña de los almacenes.

--- fire-admin-core/src/main/java/es/gob/fire/crypto/aes/AESCipher.java
+++ fire-admin-core/src/main/java/es/gob/fire/crypto/aes/AESCipher.java
@@ -2,11 +2,9 @@ package es.gob.fire.crypto.aes;
 
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
@@ -17,7 +15,6 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import es.gob.fire.commons.log.Logger;
-import es.gob.fire.commons.utils.UtilsProviders;
 import es.gob.fire.crypto.exceptions.CipherException;
 import es.gob.fire.i18n.ICoreMessages;
 import es.gob.fire.i18n.Language;
@@ -28,9 +25,9 @@ public class AESCipher implements Serializable {
 	 * Constant that represents the parameter log.
 	 */
 	private static final Logger LOGGER = Logger.getLogger(AESCipher.class);
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -5111831850889412988L;
 
@@ -43,7 +40,7 @@ public class AESCipher implements Serializable {
 	 * Attribute that represents an instance of the class.
 	 */
 	private static AESCipher instance = null;
-	
+
 	private static final String AES_PASSWORD = "ABCDEFGHIJKLMNOP";
 
 	private static final String AES_ALGORITHM = "AES";
@@ -51,11 +48,11 @@ public class AESCipher implements Serializable {
 	private static final String AES_PADDING_ALG = "AES/ECB/PKCS5Padding";
 
 	private static final String AES_NO_PADDING_ALG = "AES/GCM/NoPadding";
-	
+
 	/**
 	 * Method that obtains an instance of the class.
 	 * @return an instance of the class.
-	 * @throws CipherException If the method fails. 
+	 * @throws CipherException If the method fails.
 	 */
 	public static synchronized AESCipher getInstance() throws CipherException {
 		if (instance == null) {
@@ -63,7 +60,7 @@ public class AESCipher implements Serializable {
 		}
 		return instance;
 	}
-	
+
 	/**
 	 * Method that inits the class.
 	 * @throws CipherException If the method fails.
@@ -71,52 +68,50 @@ public class AESCipher implements Serializable {
 	private AESCipher() throws CipherException {
 		key = new SecretKeySpec(AES_PASSWORD.getBytes(), AES_ALGORITHM);
 	}
-	
+
 	/**
 	 * Method that encrypt a message.
 	 * @param msg The message to encrypt.
 	 * @return the message encrypted.
-	 * @throws CipherException If the method fails. 
+	 * @throws CipherException If the method fails.
 	 */
-	public String encryptMessageWithBC(String msg) throws CipherException {
-	    Cipher cipher;
-	    
+	public String encryptMessageWithBC(final String msg) throws CipherException {
 	    try {
-	        cipher = Cipher.getInstance(AES_PADDING_ALG, UtilsProviders.BC_PROVIDER);
+	        //Cipher cipher = Cipher.getInstance(AES_PADDING_ALG, UtilsProviders.BC_PROVIDER);
+	    	final Cipher cipher = Cipher.getInstance(AES_PADDING_ALG);
 	        cipher.init(Cipher.ENCRYPT_MODE, key);
-	        byte[] encryptedBytes = cipher.doFinal(msg.getBytes());
+	        final byte[] encryptedBytes = cipher.doFinal(msg.getBytes(StandardCharsets.UTF_8));
 	        return Base64.getEncoder().encodeToString(encryptedBytes);
-	    } catch (NoSuchProviderException | NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+	    } catch (/* NoSuchProviderException | */NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
 	        throw new CipherException(Language.getResCoreFire(ICoreMessages.LOG_CE001));
-	    } 
+	    }
 	}
-	
+
 	/**
 	 * Method that decrypting a message.
 	 * @param msg The message to decrypt.
 	 * @return the message decrypted.
 	 * @throws CipherException If the method fails.
 	 */
-	public String decryptMessageBC(String msg) throws CipherException {
+	public String decryptMessageBC(final String msg) throws CipherException {
 		try {
-			Cipher cipher = Cipher.getInstance(AES_NO_PADDING_ALG, UtilsProviders.BC_PROVIDER);
-			IvParameterSpec ivspec = new IvParameterSpec(key.getEncoded());
+			//Cipher cipher = Cipher.getInstance(AES_NO_PADDING_ALG, UtilsProviders.BC_PROVIDER);
+			final Cipher cipher = Cipher.getInstance(AES_NO_PADDING_ALG);
+			final IvParameterSpec ivspec = new IvParameterSpec(key.getEncoded());
 			cipher.init(Cipher.DECRYPT_MODE, key, ivspec);
-			byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(msg));
+			final byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(msg));
 			return new String(decryptedBytes, StandardCharsets.UTF_8);
-		} catch (BadPaddingException e) {
+		} catch (final Exception e) {
 			try {
-				Cipher cipher = Cipher.getInstance(AES_PADDING_ALG, UtilsProviders.BC_PROVIDER);
+				//Cipher cipher = Cipher.getInstance(AES_PADDING_ALG, UtilsProviders.BC_PROVIDER);
+				final Cipher cipher = Cipher.getInstance(AES_PADDING_ALG);
 				cipher.init(Cipher.DECRYPT_MODE, key);
-				byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(msg));
+				final byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(msg));
 				return new String(decryptedBytes, StandardCharsets.UTF_8);
-			} catch (NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException | InvalidKeyException
+			} catch (NoSuchAlgorithmException | /* NoSuchProviderException | */NoSuchPaddingException | InvalidKeyException
 					| IllegalBlockSizeException | BadPaddingException e2) {
 				throw new CipherException(Language.getResCoreFire(ICoreMessages.LOG_CE001));
 			}
-		} catch (NoSuchAlgorithmException | NoSuchProviderException | NoSuchPaddingException | InvalidKeyException
-				| InvalidAlgorithmParameterException | IllegalBlockSizeException e) {
-			throw new CipherException(Language.getResCoreFire(ICoreMessages.LOG_CE001));
 		}
 	}
 }
